bd544980a1bd42aef7f97cfa5737c0287d15c63d
Alejandro Sotelo <alejandrosotelo.ie@gmail.com>
2019-05-18 19:08:08 -0500

    Rename file names

--- SuperCalculator/conf.js
+++ SuperCalculator/conf.js
@@ -1,5 +1,5 @@
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['super-calculator-spec.js','first-spec.js']
+  specs: ['0-spec.js','1-spec.js','2-spec.js']
 };
